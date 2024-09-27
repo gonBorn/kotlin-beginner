4b50ef5906df6426cc4d96ee9d1959ae32bd045d
Zeyan <zeyan.du@rea-group.com>
2024-09-27 15:43:05 +0800

    arrow associate

---
+++ src/main/java/arrow/FunApi.kt
@@ -0,0 +1,19 @@
+package arrow
+
+import io.kotest.matchers.shouldBe
+import model.User
+
+object FunApi {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val users = listOf(User(1, "zeyan"), User(2, "aaa"))
+
+        // list -> map
+        users.associateBy { it.id } shouldBe mapOf(1 to User(1, "zeyan"), 2 to User(2, "aaa"))
+        users.associateBy({it.id}, {it.name}) shouldBe mapOf(1 to "zeyan", 2 to "aaa")
+        // T -> pair
+        users.associate { it.id to it.name } shouldBe mapOf(1 to "zeyan", 2 to "aaa")
+        // key 不变
+        users.associateWith { it.name } shouldBe mapOf(User(1, "zeyan") to "zeyan", User(2, "aaa") to "aaa")
+    }
+}
